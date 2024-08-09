13c1c5c243a4c33777d51942f975ebc76c1e6683
Diego226 <goodevil226@gmail.com>
2024-08-08 22:35:18 -0500

    actualiza la vista

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 function createNode(element) {
     return document.createElement(element);
 }
@@ -16,24 +17,27 @@ fetch(url)
     return contenido.map(function(radio) {
         //creamos un div de clase card
         let div1 = document.createElement('div');
-        div1.classList.add("bg-success", "card", "m-2");
-        div1.style.width ="9rem";
+        div1.classList.add("bg-success", "card", "m-1");
+        div1.style.width ="7.5rem";
+        div1.style.padding ="0";
         //creamos otro div de clase card body
         let div = createNode('div');
         div.classList.add("card-body");
+        div.style.padding ="0";
         //agregamos la imagen
         let img = createNode('img');
         img.src = radio.LOGO;
         img.classList.add("card-img-top");
+
         //creamos h5 para el nombre de la estacion
         let h5 = createNode('h5');
         h5.classList.add("card-title", "text-center", "font-weight-bold");
         h5.textContent = radio.ESTACION;
 
         let div3 = document.createElement('div');
-        div3.classList.add("d-flex","justify-content-center");
+        div3.classList.add("d-flex","justify-content-center","hola");
         let a = createNode('a');
-        a.classList.add("btn", "btn-primary","align-items-end");
+        a.classList.add("btn", "btn-primary");
         a.textContent= radio.ESTACION;
         a.id = radio.ESTACION;
         
@@ -44,9 +48,7 @@ fetch(url)
             img_radio.src = radio.LOGO;
             nom_est.textContent = radio.ESTACION;
             a.href="#inicio";
-
-
-            
+      
             // URL del archivo de video o URL del flujo en vivo
             const videoURL = radio.ENLACE;
 
